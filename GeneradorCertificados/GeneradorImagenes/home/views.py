06454330f2fed90c8d388ec30f360372927f097b
Geovanny Rojas Lindao <geovanny_rl14@hotmail.com>
2022-06-03 23:49:00 -0500

    mensaje

--- GeneradorCertificados/GeneradorImagenes/home/views.py
+++ GeneradorCertificados/GeneradorImagenes/home/views.py
@@ -31,7 +31,7 @@ def certificado(request):
         plantilla.urlSpotify = request.POST.get("cancion")
         plantilla.save()  # ORM
         generarCertificado(id)
-        sgteTexto(id)
+        #sgteTexto(id)
         Archivo = Certificado.objects.get(documentoDatos=id)
         return render(request, "descarga.html", {"objeto": Archivo})
 
@@ -40,49 +40,63 @@ def generarCertificado(id):
     objeto = Certificado.objects.get(documentoDatos=id)
     archivo = pd.read_csv(objeto.documentoDatos, sep=";",
                         encoding='utf-8')
-    datos = archivo["Nombres"].to_list()
-    musica = archivo["Musica"].to_list()
+    nombres = archivo["Nombres"].to_list()
+    # correos = archivo["Correo"].to_list()
     tipo_letra = ImageFont.truetype(objeto.tipoLetra, objeto.tamanioTexto)
+    nombreCertificado = []
+    for i in range(len(nombres)):
+        name = nombres[i]
+        name = name.strip()
+        nombreCertificado.append(name.title())
     
-    for i in range(len(datos)):
+    for i in range(len(nombreCertificado)):
         certificado = Image.open(objeto.imagen)
         #if (objeto.urlSpotify):
-        nom = guardaImg(musica[i])
-        qr = Image.open("media/plantillas/"+nom+".png")
-        certificadoW = certificado.size[0]
-        certificadoH = certificado.size[1]
-        qrsize = (int(qr.size[0]/3.5), int(qr.size[1]/3.5))
-        qr = qr.resize(qrsize, Image.ANTIALIAS)
-        ubicacion = (int(certificadoW/2)-int(qrsize[0]/2), certificadoH-70)
-        certificado.paste(qr, ubicacion)
+        # nom = guardaImg(musica[i])
+        # qr = Image.open("media/plantillas/"+nom+".png")
+        # certificadoW = certificado.size[0]
+        # certificadoH = certificado.size[1]
+        # qrsize = (int(qr.size[0]/3.5), int(qr.size[1]/3.5))
+        # qr = qr.resize(qrsize, Image.ANTIALIAS)
+        # ubicacion = (int(certificadoW/2)-int(qrsize[0]/2), certificadoH-70)
+        # certificado.paste(qr, ubicacion)
         nuevo = ImageDraw.Draw(certificado)
         coordenadas = (objeto.coordenadaX, objeto.coordenadaY -
                        objeto.tamanioTexto+10)
         color_texto = hexToRGB(objeto.colorTexto)
-        nuevo.text(coordenadas, datos[i], fill=color_texto, font=tipo_letra,align="center")
-        certificado.save("media/generadosAutomaticos/"+datos[i]+".jpg")
-        
-
-def sgteTexto(id):
-    objeto = Certificado.objects.get(documentoDatos=id)
-    archivo = pd.read_csv(objeto.documentoDatos, sep=";",
-                        encoding='utf-8')
-    datos = archivo["Nombres"].to_list()
-    cualidades = archivo ["Cualidades"].to_list()
-    tipo_letra = ImageFont.truetype(objeto.tipoLetra,35)
-    for i in range(len(datos)):
-        certificado = Image.open("media/generadosAutomaticos/"+datos[i]+".jpg")
-        nuevo = ImageDraw.Draw(certificado)
-        coordenadas = (756,842)
-        color_texto = hexToRGB(objeto.colorTexto)
-        nuevo.text(coordenadas, cualidades[i], fill=color_texto, font=tipo_letra,align="left")
-        certificado.save("media/generadosAutomaticos/"+datos[i]+".pdf")
+        nuevo.text(coordenadas, nombreCertificado[i], fill=color_texto, font=tipo_letra, align='center')
+        certificado.save("media/generadosAutomaticos/"+nombreCertificado[i]+".pdf")
         nombreRAR = "Certificados"+str(objeto.id)+".rar"
         with ZipFile("media/"+nombreRAR, "a") as myZip:
-            myZip.write("media/generadosAutomaticos/"+datos[i]+".pdf")
+            myZip.write("media/generadosAutomaticos/"+nombreCertificado[i]+".pdf")
         archivoGenerado = ArchivosGenerados()
         archivoGenerado.nombreID = id
-        archivoGenerado.docGenerado = "generadosAutomaticos/"+datos[i]+".pdf"
+        archivoGenerado.docGenerado = "generadosAutomaticos/"+nombreCertificado[i]+".pdf"
         archivoGenerado.save()
     objeto.archivoRAR = nombreRAR
     objeto.save()
+        
+
+# def sgteTexto(id):
+#     objeto = Certificado.objects.get(documentoDatos=id)
+#     archivo = pd.read_csv(objeto.documentoDatos, sep=";",
+#                         encoding='utf-8')
+#     datos = archivo["Nombres"].to_list()
+#     cualidades = archivo ["Cualidades"].to_list()
+#     tipo_letra = ImageFont.truetype(objeto.tipoLetra,35)
+#     for i in range(len(datos)):
+#         certificado = Image.open("media/generadosAutomaticos/"+datos[i]+".jpg")
+#         nuevo = ImageDraw.Draw(certificado)
+#         coordenadas = (756,842)
+#         color_texto = hexToRGB(objeto.colorTexto)
+#         nuevo.text(coordenadas, cualidades[i], fill=color_texto, font=tipo_letra,align="left")
+#         certificado.save("media/generadosAutomaticos/"+datos[i]+".pdf")
+#         nombreRAR = "Certificados"+str(objeto.id)+".rar"
+#         with ZipFile("media/"+nombreRAR, "a") as myZip:
+#             myZip.write("media/generadosAutomaticos/"+datos[i]+".pdf")
+#         archivoGenerado = ArchivosGenerados()
+#         archivoGenerado.nombreID = id
+#         archivoGenerado.docGenerado = "generadosAutomaticos/"+datos[i]+".pdf"
+#         archivoGenerado.save()
+#     objeto.archivoRAR = nombreRAR
+#     objeto.save()
